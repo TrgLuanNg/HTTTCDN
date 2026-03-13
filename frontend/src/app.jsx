ba5901f3f3c9945c8fb7c026443b6892a6537fa1
Trọng Luân <ngluan0307@gmail.com>
2026-03-13 15:57:26 +0700

    fix lỗi nhầm địa điểm file + css

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,19 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import StorefrontHome from './pages/storefront/StoreFrontHome';
 import Cart from './pages/storefront/Cart';
 import Login from './pages/storefront/Login';
-// Import tạm các component chưa tạo để giữ chỗ
 
-const StorefrontHome = () => <h2>Trang chủ Cửa hàng Sách (Đang xây dựng)</h2>;
+// Import tạm các component chưa tạo để giữ chỗ
 const DashboardHome = () => <h2>Trang Quản trị Admin </h2>;
 const NotFound = () => <h2>404 - Không tìm thấy trang</h2>;
+const Register = () => <h2>Trang Đăng Ký (Đang xây dựng)</h2>;
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<StorefrontHome />} />
-        <Route path="/cart" element={<Cart />} /> {/* Thêm dòng này */}
+        <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<DashboardHome />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
